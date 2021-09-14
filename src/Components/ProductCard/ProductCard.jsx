258a9db4081635c6e9085b7c1a947e1e585ab817
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-09-14 12:21:16 +0545

    Worked bit on content security

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -20,7 +20,7 @@ export default function ProductCard(item) {
     <div className="productCard">
       {/* section => product card image container */}
       <div className="productCard__imgContainer">
-        <Link to="#"><img src={productImage} alt={productName} /></Link>
+        <Link to="#"><img src={productImage} alt={productName} unselectable="on"/></Link>
 
         <div
           className={`productCard__ticker ${isOutOfStock && "outOfStock"} ${
